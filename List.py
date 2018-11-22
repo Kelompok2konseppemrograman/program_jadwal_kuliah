b6a7373f82c02be26c9ca7d4d3708ac3e2e00142
nurfitriyn <45030464+nurfitriyn@users.noreply.github.com>
2018-11-22 21:43:35 +0700

    Add files via upload

---
+++ List.py
@@ -0,0 +1,7 @@
+list1 = ["senin", "selasa", "rabu", "kamis", "jum'at"]
+list2 = ["08:00-09:40", "09:45-11:25", "13:30-14:10", "13:15-16:00"]
+list3 = ["kelas 2a", "kelas 2b", "kelas 2c", "kelas 2d", "kelas 2e", "kelas 2f", "kelas 2g", "kelas 2h", "kelas 3a", "kelas 3b", "kelas 3a", "kelas 3b"]
+list4 = ["LANDUNG SUDARMANA, S.T.,M.Kom", "SITI FIATUL HASANAH, S.Pd", "ADKHAN SHOLEH, S.Si.,M.Cs", "PUJI WINAR CAHYO, S.Kom.,M.Cs", "RAGIL SURYO RAHARJO, S.Fil",
+		"INDRATI HENDRIK, S.Th", "ARIF HIMAWAN, S.Kom.,M.M.,M.", "ALFIRNA RIZQI LAHITANI, S..Kom.,M.Eng", "TITIK RAHMAWATI, S.T.,M.Cs", "ALI ASMU’I, S.Ag.,M.Pd"]
+list5 = ["LOGIKA INFORMATIKA", "BAHASA INGGRIS I", "PRAKTIKUM APLIKASI KOMPUTER I ", "KONSEP PEMEROGRAMAN", "PENDIDIKAN PANCASILA", "PENDIDIKAN AGAMA KRISTEN",
+		"DESIGN THINGKING", "PENGANTAR TEKNOLOGI INFORMASI & KOMUNIKASI", "PENGANTAR REKAYASA SOFTWARE", "PENDIDIKAN AGAMA ISLAM"]

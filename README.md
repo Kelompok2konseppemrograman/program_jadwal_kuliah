0923f1b4f6a5d1ca81d194c89a3f6799b4a8450f
MHDRizky0 <muhammadrisky1401@gmail.com>
2018-12-06 18:41:32 +0700

    versi 1.3

--- README.md
+++
@@ -1,1 +0,0 @@
-# program_jadwal_kuliah
--- master.py
+++ master.py
@@ -11,8 +11,7 @@ hari = detect_date.strftime("%A")
 myWindow = Tk()
 myWindow.title("Jadwal Kuliah")
 
-
-#Metode
+#Main function
 def main():
 	if (hari == "Monday"):
 		Label(myWindow, text="{}\n{} | {} | {}\n{}".format(List.list_hari[0], List.list_mapel[0], List.list_waktu[0], List.list_kelas[1], List.list_dosen[0]), font="none 14").pack()
@@ -33,7 +32,7 @@ def main():
 		Label(myWindow, text="--------------------------------------------", font="none 14").pack()
 		Label(myWindow, text="{} | {} | {}\n{}".format(List.list_mapel[5], List.list_waktu[0], List.list_kelas[6], List.list_dosen[5]), font="none 14").pack()
 		Label(myWindow, text="--------------------------------------------", font="none 14").pack()
-		Label(myWindow, text="{} | {} | {}\n{}".format(List.list_mapel[10], List.list_waktu[3], List.list_kelas[4], List.list_dosen[10]), font="none 14").pack()
+		Label(myWindow, text="{} | {} | {}\n{}\n".format(List.list_mapel[10], List.list_waktu[3], List.list_kelas[4], List.list_dosen[10]), font="none 14").pack()
 	elif(hari == "Friday"):
 		Label(myWindow, text="{}\n{} | {} | {}\n{}".format(List.list_hari[4], List.list_mapel[7], List.list_waktu[0], List.list_kelas[2], List.list_dosen[7]), font="none 14").pack()
 		Label(myWindow, text="--------------------------------------------", font="none 14").pack()
@@ -46,49 +45,55 @@ def main():
 Label(myWindow, text="JADWAL HARI INI", font="none 16", relief="sunken").pack()
 main()
 
-#Label display jadwal hari yang dicari
-Label(myWindow, text=" ").pack()
+#search box function
 def box():
-	input_ = entry_1.get()
-	display = "JADWAL HARI " + input_.upper()
-	label_display = Label(myWindow, font="none 16", relief="sunken")
-	label_display["text"] = display
-	label_display.pack()
-	if (input_.upper() == "SENIN"):
-		Label(myWindow, text="{}\n{} | {} | {}\n{}".format(List.list_hari[0], List.list_mapel[0], List.list_waktu[0], List.list_kelas[1], List.list_dosen[0]), font="none 14").pack()
-		Label(myWindow, text="--------------------------------------------", font="none 14").pack()
-		Label(myWindow, text="{} | {} | {}\n{}".format(List.list_mapel[1], List.list_waktu[1], List.list_kelas[0], List.list_dosen[1]), font="none 14").pack()
-		Label(myWindow, text="--------------------------------------------", font="none 14").pack()
-		Label(myWindow, text="{} | {} | {}\n{}".format(List.list_mapel[2], List.list_waktu[2], List.list_kelas[9], List.list_dosen[2]), font="none 14").pack()
-	elif(input_.upper() == "SELASA"):
-		Label(myWindow, text="{}\n{} | {} | {}\n{}".format(List.list_hari[1], List.list_mapel[3], List.list_waktu[0], List.list_kelas[8], List.list_dosen[3]), font="none 14").pack()
-		Label(myWindow, text="--------------------------------------------", font="none 14").pack()
-		Label(myWindow, text="{} | {} | {}\n{}".format(List.list_mapel[1], List.list_waktu[1], List.list_kelas[0], List.list_dosen[1]), font="none 14").pack()
-	elif(input_.upper() == "RABU"):
-		Label(myWindow, text="{}\n{} | {} | {}\n{}".format(List.list_hari[2], List.list_mapel[3], List.list_waktu[0], List.list_kelas[8], List.list_dosen[3]), font="none 14").pack()
-		Label(myWindow, text="--------------------------------------------", font="none 14").pack()
-		Label(myWindow, text="{} | {} | {}\n{}".format(List.list_mapel[4], List.list_waktu[1], List.list_kelas[0], List.list_dosen[4]), font="none 14").pack()
-	elif(input_.upper() == "KAMIS"):
-		Label(myWindow, text="{}\n{} | {} | {}\n{}".format(List.list_hari[3], List.list_mapel[6], List.list_waktu[1], List.list_kelas[5], List.list_dosen[6]), font="none 14").pack()
-		Label(myWindow, text="--------------------------------------------", font="none 14").pack()
-		Label(myWindow, text="{} | {} | {}\n{}".format(List.list_mapel[5], List.list_waktu[0], List.list_kelas[6], List.list_dosen[5]), font="none 14").pack()
-		Label(myWindow, text="--------------------------------------------", font="none 14").pack()
-		Label(myWindow, text="{} | {} | {}\n{}".format(List.list_mapel[10], List.list_waktu[3], List.list_kelas[4], List.list_dosen[10]), font="none 14").pack()
-	elif(input_.upper() == "JUMAT"):
-		Label(myWindow, text="{}\n{} | {} | {}\n{}".format(List.list_hari[4], List.list_mapel[7], List.list_waktu[0], List.list_kelas[2], List.list_dosen[7]), font="none 14").pack()
-		Label(myWindow, text="--------------------------------------------", font="none 14").pack()
-		Label(myWindow, text="{} | {} | {}\n{}".format(List.list_mapel[8], List.list_waktu[1], List.list_kelas[2], List.list_dosen[8]), font="none 14").pack()
-		Label(myWindow, text="--------------------------------------------", font="none 14").pack()
-		Label(myWindow, text="{} | {} | {}\n{}".format(List.list_mapel[9], List.list_waktu[3], List.list_kelas[0], List.list_dosen[9]), font="none 14").pack()
+    input_ = entry_1.get().upper()
+    sunken = "JADWAL HARI " + input_.upper()
+    display = ""
+    if input_ == "SENIN":
+        display +="\n{}\n{} | {} | {}\n{}".format(List.list_hari[0], List.list_mapel[0], List.list_waktu[0], List.list_kelas[1], List.list_dosen[0])
+        display +="\n--------------------------------------------"
+        display +="\n{} | {} | {}\n{}".format(List.list_mapel[1], List.list_waktu[1], List.list_kelas[0], List.list_dosen[1])
+        display +="\n--------------------------------------------"
+        display +="\n{} | {} | {}\n{}".format(List.list_mapel[2], List.list_waktu[2], List.list_kelas[9], List.list_dosen[2])
+    elif input_ == "SELASA":
+    	display +="\n{}\n{} | {} | {}\n{}".format(List.list_hari[1], List.list_mapel[3], List.list_waktu[0], List.list_kelas[8], List.list_dosen[3])
+    	display +="\n--------------------------------------------"
+    	display +="\n{} | {} | {}\n{}".format(List.list_mapel[1], List.list_waktu[1], List.list_kelas[0], List.list_dosen[1])
+    elif input_ == "RABU":
+        display +="\n{}\n{} | {} | {}\n{}".format(List.list_hari[2], List.list_mapel[3], List.list_waktu[0], List.list_kelas[8], List.list_dosen[3])
+        display +="\n--------------------------------------------"
+        display +="\n{} | {} | {}\n{}".format(List.list_mapel[4], List.list_waktu[1], List.list_kelas[0], List.list_dosen[4])
+    elif input_ == "KAMIS":
+    	display +="\n{}\n{} | {} | {}\n{}".format(List.list_hari[3], List.list_mapel[6], List.list_waktu[1], List.list_kelas[5], List.list_dosen[6])
+    	display +="\n--------------------------------------------"
+    	display +="\n{} | {} | {}\n{}".format(List.list_mapel[5], List.list_waktu[0], List.list_kelas[6], List.list_dosen[5])
+    	display +="\n--------------------------------------------"
+    	display +="\n{} | {} | {}\n{}\n".format(List.list_mapel[10], List.list_waktu[3], List.list_kelas[4], List.list_dosen[10])
+    else:
+    	display +="\n{}\n{} | {} | {}\n{}".format(List.list_hari[4], List.list_mapel[7], List.list_waktu[0], List.list_kelas[2], List.list_dosen[7])
+    	display +="\n--------------------------------------------"
+    	display +="\n{} | {} | {}\n{}".format(List.list_mapel[8], List.list_waktu[1], List.list_kelas[2], List.list_dosen[8])
+    	display +="\n--------------------------------------------"
+    	display +="\n{} | {} | {}\n{}".format(List.list_mapel[9], List.list_waktu[3], List.list_kelas[0], List.list_dosen[9])
+    	
+    sunken_label['text'] = sunken # update label 
+    message['text'] = display     # update label
+    entry_1.delete(0, END)        # clear entry
 
-#Fungsi entry dan button
+#entry & button
 entry_1 = Entry(myWindow)
 button_1 = Button(myWindow, text="cari jadwal", command=box)
 
-
-#Deklarasi fungsi entry dan button
+#deklarasi entry & button
 entry_1.pack()
 button_1.pack()
+
+#deklarasi box function 
+sunken_label = Label(myWindow, text="", font="none 16", relief="sunken")
+sunken_label.pack()
+message = Label(myWindow, text="", font="none 14")
+message.pack()
 
 #Ukuran window yang ditampilkan		
 myWindow.geometry("900x900")
